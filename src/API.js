7c44507e722351ee1c6d8cd9921a55bd1f100e8e
Granit <1>
2023-07-30 22:57:41 +0400

    refactor

--- src/API.js
+++ src/API.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-import { Component } from 'react';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ODRiNmNiZjY0Y2QyMjM0YzkzMzFhZjEwNmQxODllMyIsInN1YiI6IjY0YzI0YTMxMWNmZTNhMGViNDI5YjYwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.S300Bkc2BidjY9F9z2Ty6B8glEEcCa_8ePhoL6KJ8Y0';
@@ -7,13 +5,6 @@ const API_KEY = '984b6cbf64cd2234c9331af106d189e3';
 
 const BASE_URL_FILMS = 'https://api.themoviedb.org/3';
 const POSTER_URL = 'https://image.tmdb.org/t/p/w300/';
-
-// export const getPosterFilm = async (posterPath) => {
-//   const resp = await axios.get(POSTER_URL + posterPath);
-//   console.log(resp);
-// };
-
-// getPosterFilm('rktDFPbfHfUbArZ6OOOKsXcv0Bm.jpg');
 
 export const api = createApi({
   reducerPath: 'filmsApi',
@@ -26,29 +17,9 @@ export const api = createApi({
   }),
   endpoints: (builder) => ({
     getPopularFilms: builder.query({
-      query: () => '/discover/movie',
+      query: (page) => `/discover/movie?page=${page}`,
     }),
   }),
 });
 
 export const { useGetPopularFilmsQuery } = api;
-
-// class API extends Component {
-//   constructor() {
-//     super();
-//     this.instance = axios.create({
-//       baseURL: 'https://api.themoviedb.org/3',
-//       headers: {
-//         Accept: 'application/json',
-//         Authorization: `Bearer ${TOKEN}`,
-//       },
-//     });
-//     this.imageURL = 'https://image.tmdb.org/t/p/w300/';
-//   }
-
-//   getPopularFilms = () => this.instance.get('/discover/movie');
-
-//   getPosterFilm = (posterPath) => axios.get(this.imageURL`${posterPath}`);
-// }
-
-// export const api = new API();
