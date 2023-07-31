8b42d5e49b6825fa066f125b2b38d6a7279e827b
Granit <1>
2023-07-31 23:27:49 +0400

    categories films and other

--- src/components/FilmCard/FilmCard.jsx
+++ src/components/FilmCard/FilmCard.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable camelcase */
+import no_image from '../../assets/images/no_image.png';
+
 const POSTER_URL = 'https://image.tmdb.org/t/p/w300';
 
 export const FilmCard = ({ film }) => {
@@ -9,7 +11,7 @@ export const FilmCard = ({ film }) => {
   return (
     <div className="card__wrapper">
       <div className="card__poster">
-        <img className="card__images" src={POSTER_URL + poster_path} alt="poster-films" />
+        <img className="card__images" src={poster_path ? POSTER_URL + poster_path : no_image} alt="poster-films" />
       </div>
       <div className="card__vote-average">{vote_average}</div>
       <div className="card__title">{title}</div>
--- src/tools/utils.js
+++ src/tools/utils.js
@@ -1,1 +1,27 @@
 export const pages = ['Фильмы', 'Сериалы', 'Люди'];
+export const categories = ['popular', 'now playing', 'top rating', 'upcoming'];
+
+export const changeTotalPage = (response, countPages) => ({
+  ...response,
+  data: {
+    ...response.data,
+    total_pages: countPages,
+  },
+});
+
+export const getFilms = (activeBtnCategories, dataFromDB) => {
+  const [popularFilms, nowPlayingFilms, topRatingFilms, upcomingFilms] = dataFromDB;
+
+  switch (activeBtnCategories) {
+    case 'popular':
+      return changeTotalPage(popularFilms, 500);
+    case 'now playing':
+      return nowPlayingFilms;
+    case 'top rating':
+      return changeTotalPage(topRatingFilms, 500);
+    case 'upcoming':
+      return upcomingFilms;
+    default:
+      return popularFilms;
+  }
+};
